510b024fb0cb99c459e73b8514d6e8df6d183aa2
GaletiVamsiKrishna <galetivamsikrishna@gmail.com>
2025-04-14 08:55:15 +0530

    added conditional rendering to Pizza, Menu and Footer components

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -3,16 +3,35 @@ import Pizza from './Pizza'
 import { pizzaData } from '../data'
 
 export default function Menu() {
+  const pizzas = pizzaData;
+  // const pizzas = [];
+  const numPizzas = pizzas.length;
+
   return (
     <main className='menu'>
       <h2>Our Menu</h2>
-      {/* <Pizza pizzaData={pizzaData}/> */}
-      <ul className='pizzas'>
+
+      {/* ShortCircuitOperator */}
+      {/* {
+        numPizzas > 0 && <ul className='pizzas'>
         {
-          pizzaData.map((pizza)=> <Pizza pizzaObj = {pizza}  key={pizza.name}/>
+          pizzas.map((pizza)=> <Pizza pizzaObj = {pizza}  key={pizza.name}/>
           )
         }
       </ul>
+      } */}
+
+      {/* Terniary Operator */}
+      {
+        numPizzas > 0 ? (<ul className='pizzas'>
+        {
+          pizzas.map((pizza)=> <Pizza pizzaObj = {pizza}  key={pizza.name}/>
+          )
+        }
+      </ul>): (<p>We're stil working on our menu. Please come back later</p>)
+      }
+      {/* <Pizza pizzaData={pizzaData}/> */}
+      
     </main>
   )
 }
--- src/components/Pizza.jsx
+++ src/components/Pizza.jsx
@@ -1,6 +1,12 @@
 import React from 'react'
 
 export default function Pizza({pizzaObj}) {
+
+  // If you wanted to load the entire component go with if else conditional rendering
+
+  // If else method :
+  if(pizzaObj.soldOut) return null;
+  
   return (
     <li className='pizza'>
       <img src={pizzaObj.photoName} alt={pizzaObj.name} />
