0a9d60da8ca5a4288988788c335475bbe73a4197
GaletiVamsiKrishna <galetivamsikrishna@gmail.com>
2025-04-17 10:28:33 +0530

    PizzaComponentChanges

--- src/components/Pizza.jsx
+++ src/components/Pizza.jsx
@@ -5,19 +5,17 @@ export default function Pizza({pizzaObj}) {
   // If you wanted to load the entire component go with if else conditional rendering
 
   // If else method :
-  if(pizzaObj.soldOut) return null;
+  // if(pizzaObj.soldOut) return null;
   
   return (
-    <li className='pizza'>
+    <li className={`pizza ${pizzaObj.soldOut ? "sold-out" : ""}`}>
       <img src={pizzaObj.photoName} alt={pizzaObj.name} />
          
             <div>
               <h3>{pizzaObj.name}</h3>
               <p>{pizzaObj.ingredients}</p>
-              <span>{pizzaObj.price}</span>
+              <span>{pizzaObj.soldOut ? "soldOut" : pizzaObj.price}</span>
             </div>
-          
- 
     </li>
   )
 }
